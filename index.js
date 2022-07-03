8132ff223b91d0d64df7ef9ac7655586cc88b4e4
Adolligit <adelson.onod@gmail.com>
2022-07-03 03:27:01 -0300

    Não faça como achar melhor, faça para passar no teste da Trybe!

--- index.js
+++ index.js
@@ -2,26 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 
+const app = express();
+app.use(bodyParser.json());
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 const endTalker = '/talker';
 // const endLogin = '/login';
 
-const arrPromise = fs
-  .readFile('talker.json', 'utf8')
-  .then((data) => data)
-  .catch((e) => console.log('Erro durante a leitura. \nErro:', e));
-
-const app = express();
-app.use(bodyParser.json());
+function read() {
+  return fs
+    .readFile('talker.json', 'utf8')
+    .then((data) => data)
+    .catch((e) => console.error('Erro durante a leitura do arquivo.\nErro:', e));
+}
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get(endTalker, async (req, res) => {
-  const data = await arrPromise;
+app.get(endTalker, async (_req, res) => {
+  const data = await read();
 
   return res
     .status(HTTP_OK_STATUS)
