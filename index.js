a5c1e891248943c3d19919cfa504715d11118eef
Adolligit <adelson.onod@gmail.com>
2022-07-03 01:33:50 -0300

    Retornando arquivo 'talker.json' com status 200

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
+const fs = require('fs').promises;
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const endTalker = '/talker';
+const endLogin = '/login';
+
+const arrPromise = fs
+  .readFile('talker.json', 'utf8')
+  .then((data) => data)
+  .catch((e) => console.log('Erro durante a leitura. \nErro:', e));
+
+// const content = fs.readFileSync('talker.json', 'utf8')
+
+const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
+app.get(endTalker, async (req, res) => {
+  const data = await arrPromise;
+
+  return res
+    .status(HTTP_OK_STATUS)
+    .json((data.length) ? JSON.parse(data, null, 2) : []);
 });
+
+app.listen(PORT, console.log('Online'));
